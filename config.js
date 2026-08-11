4f5c8b74daee527f060b60febcb6e40637647aba
DyshaDyshez <70228682+DyshaDyshez@users.noreply.github.com>
2026-08-11 21:53:28 +0300

    dev

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyCz0vGpRTOJxxiLzQU93PN34pvYhuUpxno",
+    authDomain: "justlink-task.firebaseapp.com",
+    databaseURL: "https://justlink-task-default-rtdb.firebaseio.com",
+    projectId: "justlink-task",
+    storageBucket: "justlink-task.firebasestorage.app",
+    messagingSenderId: "330324597396",
+    appId: "1:330324597396:web:fce9b687c11d7c8d2e7281"
+};
+
+export { firebaseConfig };
